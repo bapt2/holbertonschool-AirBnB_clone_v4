feb5a08db781386cfe7148d32ae07a781f501f77
bapt2 <5711@holbertonstudents.com>
2023-04-24 18:01:34 +0200

    adding a part of the api status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    let amenities = {};
+  
+    $('input[type="checkbox"]').change(function() {
+      if ($(this).prop('checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      let text = '';
+      for (let amenity in amenities) {
+        text += amenities[amenity] + ', ';
+      }
+      text = text.slice(0, -2); // remove last comma and space
+      $('div.amenities h4').text(text);
+      
+      $('div#api_status').
+      if($status  "OK") {
+        (this).addClass('available')
+      } else {
+        (this).removeClass('available')
+      }
+    });
+  });
