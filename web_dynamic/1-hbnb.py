9891a44100bb35abe5df39bf51f87237166f85aa
EXTRADEEN <dinumira@gmail.com>
2023-04-24 18:42:49 +0200

    fixed code

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -7,8 +7,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-app = Flask(__name__)
 import uuid
+app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -38,7 +38,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
--- web_dynamic/static/scripts/1-hbnh.js
+++ web_dynamic/static/scripts/1-hbnh.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    let amenities = {};
-  
-    $('input[type="checkbox"]').change(function() {
-      if ($(this).prop('checked')) {
-        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete amenities[$(this).attr('data-id')];
-      }
-      let text = '';
-      for (let amenity in amenities) {
-        text += amenities[amenity] + ', ';
-      }
-      text = text.slice(0, -2); // remove last comma and space
-      $('div.amenities h4').text(text);
-    });
+  let amenities = {};
+
+  $('input[type="checkbox"]').change(function() {
+    if ($(this).prop('checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    let text = '';
+    for (let amenity in amenities) {
+      text += amenities[amenity] + ', ';
+    }
+    text = text.slice(0, -2); // remove last comma and space
+    $('div.amenities h4').text(text);
   });
+});
