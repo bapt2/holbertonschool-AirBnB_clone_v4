1abd52bc06becbed37e7124233525d424b8fdf09
EXTRADEEN <dinumira@gmail.com>
2023-04-25 19:36:30 +0200

    fixed issue

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,27 +1,26 @@
-$(document).ready(function() {
-    let amenities = {};
-  
-    $('input[type="checkbox"]').change(function() {
-      if ($(this).prop('checked')) {
-        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete amenities[$(this).attr('data-id')];
-      }
-      let text = '';
-      for (let amenity in amenities) {
-        text += amenities[amenity] + ', ';
-      }
-      text = text.slice(0, -2); // remove last comma and space
-      $('div.amenities h4').text(text);      
-    });
-  });
+$(document).ready(function () {
+  const amenities = {};
 
-  $(document).ready(function() {
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-        if($(data.status = 'OK')) {
-          ('DIV#api_status').addClass('available')
-        } else {
-          ('DIV#api_status').removeClass('available')
-        }
-      })
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).prop('checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    let text = '';
+    for (const amenity in amenities) {
+      text += amenities[amenity] + ', ';
+    }
+    text = text.slice(0, -2); // remove last comma and space
+    $('div.amenities h4').text(text);
+  });
+});
+
+const APIURL = 'http://0.0.0.0:5000/api/v1/status';
+  $.get(APIURL, function (data, response) {
+    if (response === 'success' && data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
   });
