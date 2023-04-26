35a816a608afe5fa34350a9f743949c3699163ab
bapt2 <5711@holbertonstudents.com>
2023-04-26 09:08:25 +0200

    adding a part of the task

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    const amenities = {};
+  
+    $('input[type="checkbox"]').change(function () {
+      if ($(this).prop('checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      let text = '';
+      for (const amenity in amenities) {
+        text += amenities[amenity] + ', ';
+      }
+      text = text.slice(0, -2); // remove last comma and space
+      $('div.amenities h4').text(text);
+    });
+  });
+  
+  const APIURL = 'http://0.0.0.0:5000/api/v1/status';
+    $.get(APIURL, function (data, response) {
+      if (response === 'success' && data.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
+  
+  const places = {}
+  const Url = "http://0.0.0.0:5001/api/v1/places_search"
+  const Data = "Content-Type: application/json"
+  $.post(Url, Data, function(data, response) {
+    $.each()
+  })
